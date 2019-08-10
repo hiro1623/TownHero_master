4f63cb5bf1aa466fe65aae14fca3f0e3839a9380
Giraffe735 <okaran.2101@gmail.com>
2019-08-11 03:03:06 +0900

    Add: 簡易的な投稿表示機能
        - 関数"distance"を追加(map.js)
        - テスト配列"posts"に閲覧範囲内投稿を追加(map.js)
        - 閲覧範囲内外で投稿の挙動が変わるよう変更(map.js)
        - 閲覧範囲内外それぞれのマーカーセレクタを追加(map.css)
    
    全commitでは配列内に格納された投稿をそのまま表示していたため，閲覧制限機能と投稿マーカーの色（閲覧可否の判別用）を追加．データベース実装前だが土台として．
    
    ただし，閲覧制限はJSの書き換えで簡単に解除できる．この方式については要検討．

--- TownHero/TownHero/static/script/map.js
+++ TownHero/TownHero/static/script/map.js
@@ -5,12 +5,13 @@
   function init() {
     navigator.geolocation.getCurrentPosition(function succes_func(position) {
 
-      // [TEST-01] 投稿表示テスト用配列 [日時, userID, 緯度, 経度, 投稿内容]
+      // [TEST-01] 投稿表示テスト用配列 [日時, 投稿属性, userID, 緯度, 経度, 投稿内容]
       //           とりあえず投稿内容なども表示しているが，アクセス制限の観点からこの時点ではまだ受け取るべきじゃない感
       var posts = [
-        ["20190807", "testUser01", 34.236312, 132.601736, "これは投稿表示テスト用1の文章です．藤三"],
-        ["20190808", "testUser02", 34.228246, 132.602541, "これは投稿表示テスト用2の文章です．阿賀端"],
-        ["20190809", "testUser03", 34.232259, 132.600007, "これは投稿表示テスト用3の文章です．高専陸上"]
+        ["20190807", "testUser01", 1, 34.236312, 132.601736, "これは投稿表示テスト用1の文章です．藤三"],
+        ["20190808", "testUser02", 1, 34.228246, 132.602541, "これは投稿表示テスト用2の文章です．阿賀端"],
+        ["20190809", "testUser03", 2, 34.232259, 132.600007, "これは投稿表示テスト用3の文章です．高専陸上"],
+        ["20190810", "testUser04", 1, 34.231081, 132.602176, "これは投稿表示テスト用4の文章です．活動拠点"]
       ];
       // [TEST-01]
 
@@ -22,8 +23,8 @@
       var map = L.map('mapcontainer', {zoomControl: false});
       var mpoint = [lat, lng]
       map.setView(mpoint, 16);
-      
-      
+
+
       // マーカー表示
       var pulsingIcon = L.icon.pulse({
         iconColor: '#87cefa',
@@ -41,8 +42,8 @@
       L.marker(mpoint,{icon: pulsingIcon}).addTo(map);
       //L.control.scale({maxWidth: 200, position: 'bottomright', imperial: false}).addTo(map);
       //L.control.zoom({position: 'bottomleft'}).addTo(map);
-      
-      
+
+
       // 地図表示
       //地理院地図の標準地図タイル
       var gsi = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'></a>"});
@@ -61,12 +62,15 @@
       /* L.control.layers(baseMaps).addTo(map); */
       osm.addTo(map);
 
-      // [TEST-01] 投稿表示機能 [日時, userID, 緯度, 経度, 投稿内容]
+      // [TEST-01] 投稿表示機能 [日時, userID, 属性, 緯度, 経度, 投稿内容]
       for (var i=0; i<posts.length; i++){
-        L.marker([posts[i][2], posts[i][3]],{icon: L.divIcon}).bindPopup(posts[i][0] + "\n" + posts[i][4]).addTo(map);
-      }
-      // [TEST-01]
-      
+        if (0.25 >= distance(lat, lng, posts[i][3], posts[i][4])){
+          L.marker([posts[i][3], posts[i][4]], {icon: L.divIcon({className: 'activeMarker'})}).bindPopup(posts[i][0] + "<br>" + posts[i][5]).addTo(map);
+        } else {
+          L.marker([posts[i][3], posts[i][4]], {icon: L.divIcon({className: 'deactiveMarker'})}).addTo(map);
+        }
+      } // [TEST-01]
+
       // 地図のclickイベントでonMapClick関数を呼び出し
       map.on('click', onMapClick);
 
@@ -79,8 +83,18 @@
         //マーカーのclickイベント呼び出される クリックされたマーカーを地図のレイヤから削除する
         map.removeLayer(e.target);
       }
-      
-    
+
+
+    // 内部関数
+    // 関数： 2点間の距離を算出
+    function distance(lat1, lng1, lat2, lng2) {
+      lat1 *= Math.PI / 180;
+      lng1 *= Math.PI / 180;
+      lat2 *= Math.PI / 180;
+      lng2 *= Math.PI / 180;
+      return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+    }
+
     // error messege
     }, function (error) {
       switch (error.code) {
