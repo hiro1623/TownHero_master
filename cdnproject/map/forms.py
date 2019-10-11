9a6b2031e2e3998a3d3b603a76ba26e7f309be5f
Kotaro <jascine@icloud.com>
2019-10-12 06:54:08 +0900

    履歴画面で画像の表示（元々のサイズなので改善の必要あり）

--- cdnproject/map/forms.py
+++ cdnproject/map/forms.py
@@ -1,3 +1,29 @@
 from django import forms
 
+from .models import PostData
+from django.contrib.auth.models import User
+
+POST_CHOICE = (
+    ('0','テストだよ'),
+    ('1','テスト２号だよ')
+)
+class PostingForm(forms.Form):
+
+        message = forms.CharField(
+            label='メッセージ',
+            max_length=200,
+            required=True,
+            widget=forms.Textarea(
+                attrs={'rows':5,'placeholder':'What is on your mind?'}
+            )
+        )
+        purpose = forms.ChoiceField(
+            label='投稿の種類',
+            widget=forms.Select,
+            choices = POST_CHOICE,
+            required=True,
+        )
+        pic = forms.ImageField(
+            label='写真',
+        )
 
