4a3931652f95eba3bbeb96e264d92fa71fe54cba
Giraffe735 <okaran.2101@gmail.com>
2019-10-13 06:04:18 +0900

    統合6:04

--- cdnproject/cdnproject/urls.py
+++ cdnproject/cdnproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path("",
@@ -25,5 +28,4 @@ urlpatterns = [
     path('map/', include('map.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
